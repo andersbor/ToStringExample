5d9ffeaf1f3c4d972ae594a8d9b73c4dbb5fd14e
Anders Børjesson <anbo@zealand.dk>
2021-11-01 19:48:12 +0100

    Add project files.

---
+++ toStringExa/Athlete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace toStringExa
+{
+    class Athlete: Object 
+    // Klassen arver altid fra object, men normalt skriver vi det ikke eksplicit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Height { get; set; }
+
+        // override: Jeg laver min egen version af den ToString metode der er i base klassen (Object)
+        public override string ToString()
+        {
+            // return base.ToString();
+            // FORKERT: Min ToString() kalder base-klassen (Object) ToString,
+            // som producerer en uønsket string.
+            
+            //return ToString();
+            // FORKERT: ToString kalder sig selv uden ende. Det ender med en stack overflow exception
+            return Id + " " + Name + " " + Country + " " + Height;
+        }
+    }
+}
---
+++ toStringExa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace toStringExa
+{
+    class Program
+    {
+        static void Main()
+        {
+            Athlete ath = new Athlete
+            {
+                Id = 1, Name = "Anders", Country = "DK", Height = 198
+            };
+            Console.WriteLine(ath.ToString());
+
+            Object ath2 = new Athlete
+            {
+                Id = 2,
+                Name = "Kurt",
+                Country = "DK",
+                Height = 168
+            };
+            Console.WriteLine(ath2.ToString());
+            // Kalder Athlete.ToString()
+            // Typen på ath2 (Object) bestemmer på compile-tid hvilke metoder vi må kalde (kun dem der er defineret i klassen Object)
+            // På runtime bestemmer objektet (new Athlete) hvilke version af ToString metoden der kaldes.
+            // Kaldes polymorfisme.
+            // Keyword "override" på ToString er vigtigt - eller virker polymorfisme ikke.
+        }
+    }
+}
